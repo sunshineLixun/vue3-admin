504a2123e82968467b8c86a704ee76b46ca808d0
li <li@lideMacBook-Air.local>
2022-05-20 09:23:31 +0800

    引入antd

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Antd from "ant-design-vue";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(Antd);
 
 app.mount("#app");
