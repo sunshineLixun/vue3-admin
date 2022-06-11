0a27f43d6437b399efaa1a8bc28c520e406c5e51
Hello Li <1261142605@qq.com>
2022-06-11 16:21:59 +0800

    feat: add menu-fold

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,11 +1,23 @@
 import { defineComponent } from "vue";
-import { Layout } from "ant-design-vue";
+import { Layout, Space } from "ant-design-vue";
+import { MenuFoldOutlined } from "@ant-design/icons-vue";
 import styles from "./index.module.scss";
 
 const Header = defineComponent({
 	setup() {
+		const onMenuFold = () => {
+			console.log("fold");
+		};
 		return () => {
-			return <Layout.Header class={styles.header}>Header</Layout.Header>;
+			return (
+				<Layout.Header class={styles.header}>
+					<Space size={20}>
+						<solt>
+							<MenuFoldOutlined class={styles["out-line"]} onClick={onMenuFold} />
+						</solt>
+					</Space>
+				</Layout.Header>
+			);
 		};
 	}
 });
