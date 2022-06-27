597ccbc6f19564dde80cfdd28b0204e6e9c684a7
Hello Li <1261142605@qq.com>
2022-06-27 16:53:52 +0800

    feat: add mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,66 @@
 import path from "path";
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import type { UserConfigExport, ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { createStyleImportPlugin, AndDesignVueResolve } from "vite-plugin-style-import";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import { viteMockServe } from "vite-plugin-mock";
 // import Components from "unplugin-vue-components/vite";
 // import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	// 1. 如果使用的是ant-design 系列的 需要配置这个
-	// 2. 确保less安装在依赖 `yarn add less -D`
-	css: {
-		preprocessorOptions: {
-			less: {
-				javascriptEnabled: true
-			}
-		}
-	},
-	plugins: [
-		vue(),
-		vueJsx(),
-		createStyleImportPlugin({
-			resolves: [AndDesignVueResolve()]
-		}),
-		vueI18n({
-			// if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-			// compositionOnly: false,
+export default ({ command }: ConfigEnv): UserConfigExport => {
+	const isBuild = command === "build";
 
-			// you need to set i18n resource including paths !
-			// fix：https://github.com/intlify/vue-i18n-next/issues/789
-			include: path.resolve(__dirname, "/src/locales/lang/json/**")
-		})
-		// Components({
-		// 	resolvers: [AntDesignVueResolver()]
-		// })
-	],
-	server: {
-		open: true,
-		proxy: {
-			"/api": {
-				target: "https://www.dgtle.com",
-				changeOrigin: true,
-				rewrite: path => path.replace(/^\/api/, "")
+	return {
+		// 1. 如果使用的是ant-design 系列的 需要配置这个
+		// 2. 确保less安装在依赖 `yarn add less -D`
+		css: {
+			preprocessorOptions: {
+				less: {
+					javascriptEnabled: true
+				}
+			}
+		},
+		plugins: [
+			vue(),
+			vueJsx(),
+			viteMockServe({
+				mockPath: "mock",
+				localEnabled: !isBuild,
+				prodEnabled: isBuild
+			}),
+			createStyleImportPlugin({
+				resolves: [AndDesignVueResolve()]
+			}),
+			vueI18n({
+				// if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+				// compositionOnly: false,
+
+				// you need to set i18n resource including paths !
+				// fix：https://github.com/intlify/vue-i18n-next/issues/789
+				include: path.resolve(__dirname, "/src/locales/lang/json/**")
+			})
+			// Components({
+			// 	resolvers: [AntDesignVueResolver()]
+			// })
+		],
+		server: {
+			open: true,
+			proxy: {
+				"/api": {
+					target: "https://www.dgtle.com",
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api/, "")
+				}
+			}
+		},
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url))
 			}
 		}
-	},
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url))
-		}
-	}
-});
+	};
+};
