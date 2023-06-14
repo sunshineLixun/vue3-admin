62341a854e6d8f2b9faf162826d438e3a7b5ebdb
Hello Li <1261142605@qq.com>
2023-06-14 14:32:10 +0800

    feat: 引入@ant-design-vue/pro-form

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
 import { ConfigProvider } from "ant-design-vue";
 import { useI18nv } from "@/hooks/useI18n";
 const { getAntdLocale } = useI18nv();
-
 </script>
 
 <style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,8 @@
 <template>
 	{{ t("routes.dashboard.dashboard") }}
-
 </template>
 
 <script lang="ts" setup>
 import { useI18nv } from "@/hooks/useI18n";
 const { t } = useI18nv();
-
-
 </script>
