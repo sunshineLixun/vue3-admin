18bf0d8f99373d385511ccdfe6eae487d224756d
Hello Li <1261142605@qq.com>
2023-07-13 15:00:52 +0800

    feat: add UnoCSS

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,11 @@
 <template>
-	{{ t("routes.dashboard.dashboard") }}
+	<div mt-20px flex justify-center>
+		{{ t("routes.dashboard.dashboard") }}	
+	</div>
 </template>
 
 <script lang="ts" setup>
 import { useI18nv } from "@/hooks/useI18n";
 const { t } = useI18nv();
 </script>
+
--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,6 @@ import {
 	presetIcons,
 	presetTypography,
 	presetUno,
-	presetWebFonts,
 	transformerDirectives,
 	transformerVariantGroup
 } from "unocss";
@@ -20,15 +19,14 @@ export default defineConfig({
 		}
 	},
 	presets: [
+		// unocss默认解析器 必须得有
 		presetUno(),
+		// 可以直接给标签设置对应的语义化的css， 类似：<div text="white" border="2 rounded blue-300" />  推荐
 		presetAttributify(),
+		// 图片直接当svg来用，icon // 推荐
 		presetIcons(),
-		presetTypography(),
-		presetWebFonts({
-			fonts: {
-				// ...
-			}
-		})
+		// 可以直接在标签上写样式，不用写在class里面了。类似<div mr-1 /> 推荐
+		presetTypography()
 	],
 	transformers: [transformerDirectives(), transformerVariantGroup()]
 });
